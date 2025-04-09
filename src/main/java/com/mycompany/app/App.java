51a788503ea3eb9e1636d7340654c44a0a0882a9
Wei Khang <bongwk.keys@gmail.com>
2025-04-09 16:55:52 +0800

    Simulate 2nd change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "I love Jenkins, Jenkins is so cool.";
+    private static final String MESSAGE = "I love Jenkins";
 
     public App() {}
 
