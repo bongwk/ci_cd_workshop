ca09f82e36c51982e54442a1b131bf40b9989841
Wei Khang <bongwk.keys@gmail.com>
2025-04-10 09:50:23 +0800

    Some changes to test webhooks

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Hello there ! I am simple Java app.");
+        System.out.println("Hello there ! I am simple Java app. Currently I am just doing a change.");
     }
 
     public String getMessage() {
